d32ea2e3c0da1e50bbe04db3d895cd9f1c266295
Jonathan Sibley <jonsibley@gmail.com>
2013-09-29 00:21:05 -0700

    Add test flight pod

--- Podfile
+++ Podfile
@@ -2,3 +2,4 @@ platform :ios, '7.0'
 
 pod 'HTAutocompleteTextField'
 pod 'iRate'
+pod 'TestFlightSDK'
--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_HTAutocompleteTextField 2
 #define COCOAPODS_VERSION_PATCH_HTAutocompleteTextField 2
 
+// TestFlightSDK
+#define COCOAPODS_POD_AVAILABLE_TestFlightSDK
+#define COCOAPODS_VERSION_MAJOR_TestFlightSDK 2
+#define COCOAPODS_VERSION_MINOR_TestFlightSDK 0
+#define COCOAPODS_VERSION_PATCH_TestFlightSDK 0
+
 // iRate
 #define COCOAPODS_POD_AVAILABLE_iRate
 #define COCOAPODS_VERSION_MAJOR_iRate 1
