a49092dcfe58c0055667f3b4ee0f8247afac2fec
Jonathan Sibley <jonsibley@gmail.com>
2013-10-18 10:45:39 -0700

    Comment out debugging in pre-actions 2

--- scripts/GoDeep-pre-actions.sh
+++ scripts/GoDeep-pre-actions.sh
@@ -3,7 +3,7 @@
 # so we strip off /XXX.xcworkspace with sed
 project_dir=`echo "$WORKSPACE_PATH" | sed -e "s/\/[^\/]*$//"`
 
-#if [ ${USER}="ios" ]; then
+if [ ${USER}="ios" ]; then
     # Modify the build number
     ruby "$project_dir/scripts/setCommitHashAsBundleVersion.rb"
-# fi
+fi
