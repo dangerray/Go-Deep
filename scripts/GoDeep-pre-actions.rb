25c6682ff8cf404a60646cc0e3be3eb74b48a247
Jonathan Sibley <jonsibley@gmail.com>
2013-10-21 11:34:02 -0700

    Revert "Remove check for user  before running build actions."
    This reverts commit 2a0d0960e2aee907ddea1b5354cea1532b13438d.

--- scripts/GoDeep-pre-actions.rb
+++ scripts/GoDeep-pre-actions.rb
@@ -5,6 +5,6 @@ p = Pathname.new(ENV['WORKSPACE_PATH'])
 project_dir = p.dirname.to_s
 
 # Check for user "ios" to know if this is being run on our Xcode CI environment
-# if ENV['USER'] == "ios"
+if ENV['USER'] == "ios"
     `ruby #{project_dir}/scripts/setCommitHashAsBundleVersion.rb`
-# end
+end
