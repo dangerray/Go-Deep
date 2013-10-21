2a0d0960e2aee907ddea1b5354cea1532b13438d
iOS <ios@iOSs-iMac.local>
2013-10-21 09:02:20 -0700

    Remove check for user  before running build actions.

--- scripts/GoDeep-post-actions.rb
+++ scripts/GoDeep-post-actions.rb
@@ -5,6 +5,6 @@ p = Pathname.new(ENV['WORKSPACE_PATH'])
 project_dir = p.dirname.to_s
 
 # Check for user "ios" to know if this is being run on our Xcode CI environment
-if ENV['USER'] == "ios"
+# if ENV['USER'] == "ios"
     `sh #{project_dir}/scripts/Xcode4HockeyAppTestFlightintegration.sh`
-end
+# end
